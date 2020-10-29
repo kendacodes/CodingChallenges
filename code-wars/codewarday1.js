5263cc2743aa3cc2a67b4d4a391a125aed61a326
KendaCodes <K.Lamandier@gmail.com>
2020-10-28 21:13:33 -0400

    Create codewarday1.js
    Convert a number to String

---
+++ code-wars/codewarday1.js
@@ -0,0 +1,12 @@
+// CONVERT NUMBER TO STRING
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+
+function numberToString(num) {
+    return num + '';
+  }
+
+  numberToString();
