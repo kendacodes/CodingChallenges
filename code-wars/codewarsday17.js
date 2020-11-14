cc36f270af1beed083ed6f21c7cd9ba92718d599
KendaCodes <K.Lamandier@gmail.com>
2020-11-13 22:56:04 -0500

    Return the first M multiples of N

---
+++ code-wars/codewarsday17.js
@@ -0,0 +1,19 @@
+// Return the first M multiples of N
+
+/*
+Description:
+Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+Ex.
+multiples(3, 5)
+should return
+[5, 10, 15]
+*/
+
+const multiples = (m, n) => {
+  // code here
+  const arr = [];
+  for (let i = 1; i <= m; i++) {
+    arr.push(n * i)
+  }
+  return arr
+}
