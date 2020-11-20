5ad2ea9757a063caf4b9658237764f2d73658595
KendaCodes <K.Lamandier@gmail.com>
2020-11-19 22:09:15 -0500

    String ends with?

---
+++ code-wars/codewarsday22.js
@@ -0,0 +1,14 @@
+// String ends with?
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//
+// Examples:
+//
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+
+const solution = (str, ending) => {
+  const strEnding = str.substr(str.length - ending.length);
+  return strEnding === ending;
+}
